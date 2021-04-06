23f78d1cba7533bf6a85608fefcbb5b2ef57f016
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-04-06 13:17:37 -0400

    Some refactors, added

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const productsRouter = require('./routes/products');
+const productsRouter = require('./routes/views/products');
 const productsApiRouter = require('./routes/api/products');
 
+// app
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+// statics files
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// routes
 app.use('/products', productsRouter);
 app.use('/api/products', productsApiRouter);
 
+// redirect
+app.get('/', function(req, res) {
+  res.redirect('/products');
+});
 
-app.use(express.json());
-
+// server
 const server = app.listen(8000, function() {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
