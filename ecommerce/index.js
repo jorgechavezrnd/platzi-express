c33f7887a3b18c1093aa19f40ba07ebaa1b964bc
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-04-07 18:26:02 -0400

    Error handlers with middlewares, added

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,6 +3,12 @@ const path = require('path');
 const productsRouter = require('./routes/views/products');
 const productsApiRouter = require('./routes/api/products');
 
+const {
+  logErrors,
+  clientErrorHandler,
+  errorHandler
+} = require('./utils/middlewares/errorsHandlers');
+
 // app
 const app = express();
 
@@ -24,6 +30,11 @@ app.use('/api/products', productsApiRouter);
 app.get('/', function(req, res) {
   res.redirect('/products');
 });
+
+// error handlers
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 
 // server
 const server = app.listen(8000, function() {
